fb0ca7ab56ccb02e3e4e051e9a1fb4e57cb7d25c
Michael Suguitan <psychomugs@gmail.com>
2023-01-29 21:49:38 -0500

    Add conda environment.yaml

--- src/gadgets/midi_controller.py
+++ src/gadgets/midi_controller.py
@@ -17,6 +17,14 @@ EVENT_TABLE = {
 MUTE_EVENTS = [
     'clock'
 ]
+MIDO_ARGS = [
+    'type','channel',
+    'note','velocity',
+    'value',
+    'program',
+    'pitch'
+]
+
 
 class MIDIController(Gadget):
     def __init__(self, config, **kwargs):
@@ -24,7 +32,6 @@ class MIDIController(Gadget):
         self.midi_port = mido.open_ioport(
             config['port_name'],
             callback=self.midi_callback)
-        # self.echo = True
         self.echo = False
         [EVENT_TABLE.pop(mute_message,None) for mute_message in MUTE_EVENTS]
         self.message = MIDIMessage
@@ -49,16 +56,13 @@ class MIDIController(Gadget):
                     
     @load_pickle
     def on_midi(self,data):
-        data.pop('event')
-        # data.update({'echo':False})
-        # logging.debug(data)
+        logging.debug(data)
         self.midi_port.send(
             # TODO - update to self-defined MIDIMessage(**data)
             # mido.Message(**data)
             data['msg']
         )
-        # pass
-                    
+                   
     def disconnect(self):
         super().disconnect()
         self.midi_port.close()
@@ -71,41 +75,24 @@ class MIDINamespace(Namespace):
 class MIDIMessage(Message, MidoMessage):
     def __init__(self, **kwargs):
         Message.__init__(self, **kwargs)
-        # MidoMessage.__init__(self)
-        # kwargs.pop('event') 
-        mido_args = [
-            'type','channel',
-            'note','velocity',
-            'value',
-            'program',
-            'pitch'
-        ]
-        # logging.debug(kwargs)
-        mido_kwargs = {k:v for k,v in kwargs.items() if k in mido_args}
-        # print(mido_kwargs)
+        mido_kwargs = {k:v for k,v in kwargs.items() if k in MIDO_ARGS}
+        logging.debug(kwargs)
+        logging.debug(mido_kwargs)
         MidoMessage.__init__(self,**mido_kwargs)
         
     @classmethod
     def from_mido(self, mido_msg: mido.Message, **kwargs):
         self = mido_msg
-        # self.msg = mido_msg
-        # print(self.__dict__
-        # print(kwargs)
-        # Message.__init__(self, **kwargs)
-        event = EVENT_TABLE.get(mido_msg.type,'midi_cc')
-        # self.__dict__.update(self.msg.__dict__)
         mido_dict = {}
-        # # update instances with basic fields of types (str,bool,int,float)
+        # update instances with basic fields of types (str,bool,int,float)
         mido_dict.update({
             k:v for k,v in mido_msg.__dict__.items() if isinstance(
                 v,(str,bool,int,float))})
-        # # value = mido_dict.get('note',None) or mido_msg.value
-        # value = mido_dict.get('note_on' if 'note' in mido_dict['type'] else 'value',None)
-        # event = EVENT_TABLE.get(mido_msg.type,mido_msg.type)
-        # mido_dict.update(dict(
-        #     event=event,
-        #     value=value,
-        # ))
-        # return self
-        return MIDIMessage(event=event,msg=mido_msg,**mido_dict)
+        
+        return MIDIMessage(
+            event=EVENT_TABLE.get(
+                mido_msg.type,
+                'midi_cc'),            
+            msg=mido_msg,
+            **mido_dict)
         
