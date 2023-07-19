4c3db28a30ce4a370b5d413fb409140bc1cb60ee
Michael Suguitan <psychomugs@gmail.com>
2023-07-18 21:58:37 -0400

    minimized requirements

--- r0b0/kinematics/kinematics.py
+++ r0b0/kinematics/kinematics.py
@@ -10,7 +10,8 @@ class Kinematics(object):
         
     # get direction cosine matrix (R) from given euler angles
     # gives O_R_B (rotation from body frame to the inertial frame)
-    def angle2dcm(self, euler: 'np.ndarray[np.float]') -> dcm: 'np.ndarray[np.float]':
+    # def angle2dcm(self, euler: 'np.ndarray[np.float]') -> dcm: 'np.ndarray[np.float]':
+    def angle2dcm(self, euler):
         """
         Get direction cosine matrix (O_R_B) from Euler angles
         args:
